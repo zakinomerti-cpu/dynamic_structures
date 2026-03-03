b5aaf1e51b2b67de9b378dcab491f4e83cf42c27
Xinitrix <Zakinomerti@gmail.com>
2026-03-03 14:43:19 +0700

    some update

--- stdint.h
+++ stdint.h
@@ -1,8 +1,6 @@
 #ifndef _STDINT_H_
 #define _STDINT_H_
 
-#ifdef _MSC_VER
-
 typedef signed __int8       int8_t;
 typedef unsigned __int8     uint8_t;
 
@@ -14,16 +12,5 @@ typedef unsigned __int32    uint32_t;
 
 typedef signed __int64      int64_t;
 typedef unsigned __int64    uint64_t;
-
-/* pointer-sized */
-#ifdef _WIN64
-typedef signed __int64      intptr_t;
-typedef unsigned __int64    uintptr_t;
-#else
-typedef signed __int32      intptr_t;
-typedef unsigned __int32    uintptr_t;
-#endif
-
-#endif
 
 #endif
